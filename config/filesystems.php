b27b79a437ad7c2d6615a3c6576a458de5a6255b
andrisyahputra <andrisyahputra2209@gmail.com>
2023-12-26 11:45:22 +0700

    permission

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,13 +36,31 @@ return [
             'throw' => false,
         ],
 
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
             'throw' => false,
         ],
+
 
         's3' => [
             'driver' => 's3',
