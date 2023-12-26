12204a3ba3412a216eb1cd75185a9f0762ceb116
andrisyahputra <andrisyahputra2209@gmail.com>
2023-12-26 12:12:00 +0700

    permission 2

--- app/Traits/FotoTrait.php
+++ app/Traits/FotoTrait.php
@@ -28,6 +28,8 @@ trait PhotoProdukTrait
         // Store the file in the specified storage path
         $path = $file->storeAs($storagePath, $filename);
 
+        $folderPath = dirname($path);
+        $this->setFolderPermissions($folderPath);
         return $path;
     }
     private function setFolderPermissions($folderPath)
